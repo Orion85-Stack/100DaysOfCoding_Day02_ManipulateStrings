ca8ce241b0e835441a2a7a958381e746b9df5fff
Orion85-Stack <70532381+Orion85-Stack@users.noreply.github.com>
2023-05-16 20:18:59 +0000

    Inital commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Data Types")
+
+#Count from 0 in Python to find the index position
+print("Hello"[0])
+
+#first convert the numeric to str then can use the
+#len function
+print(len(str(12345869)))
